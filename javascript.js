c1703c1d01939e7d175e50e534d9de40743ea27c
Rich D <richie.donovan62@gmail.com>
2018-10-24 17:41:39 -0500

    Almost done. need to add READ ME

--- javascript.js
+++ javascript.js
@@ -9,14 +9,15 @@ var createButtons = function() {
     //create buttons based on elements in the array
     for (var i=0; i<topics.length; i++){
         //Creates new buttons
-        var newButton = $("<button class='btn'>");
+        var newButton = $("<button>");
         //Give button an attribute
         newButton.attr("data-type");
         //Give button a name that reflects the array
         newButton.text(topics[i]);
         //add class to button
-        newButton.attr("class", "gif");
+        newButton.attr("class", "gif btn");
         //add buttons to the DOM
+        newButton.attr("data-show", topics[i]);
         $("#buttons").append(newButton);
     }
 }
@@ -40,11 +41,11 @@ var submit = function() {
 }
 
 var displayGif = function(){
-    $("#buttons").on("click", function() {
+    $(document).on("click", ".gif", function() {
         var show = $(this).attr("data-show");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
           show + "&api_key=dc6zaTOxFJmzC&limit=10";
-    
+        console.log(this);
         $.ajax({
           url: queryURL,
           method: "GET"
@@ -62,14 +63,9 @@ var displayGif = function(){
     
               var showImage = $("<img>");
               showImage.attr("src", results[i].images.original_still.url);
-                // var state = "still";
-                //   if (state === "still") {
-                //     $(this).attr("src", $(this).attr(images.fixed_height.url));
-                //     state = "animated"
-                //   } else if (state === "animated") {
-                //     $(this).attr("src", $(this). attr("data-still"));
-                //     $(this).attr("data-state", "still");
-                //   }
+              showImage.attr("data-still", results[i].images.original_still.url);
+              showImage.attr("data-animated", results[i].images.original.url);
+              showImage.attr("data-state", "still");
     
               gifDiv.prepend(p);
               gifDiv.prepend(showImage);
@@ -81,9 +77,23 @@ var displayGif = function(){
 
 }
 
+var animateGif = function(){
+    $(document).on("click", "img", function(){
+        var state = $(this).attr("data-state");
+          if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animated"));
+            $(this).attr("data-state", "animated");
+          } else if (state === "animated") {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+    });
+}
+
 createButtons();
 submit();
 displayGif();
+animateGif();
 
 
 
